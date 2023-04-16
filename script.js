67ccba11e9ab20b620de8027dda61b23a4676663
ankit-yadav-17 <119732393+ankit-yadav-17@users.noreply.github.com>
2023-04-16 09:29:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function fibonacci(num) {
 // your code here
+	let first = 0;
+	let second = 1;
+
+	if(num == 0){
+		console.log(first);
+	}
+	else if(num == 1){
+		console.log(second)
+	}
+	else{
+		for(let i=2;i<=num;i++){
+			let third = first + second;
+			first = second;
+			second = third;
+			console.log(third);
+		}
+	}
 }
 
 module.exports = fibonacci;
